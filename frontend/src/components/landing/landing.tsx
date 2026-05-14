4fddd10ee72bf56234fd536ae48b785780291e9b
user.email <zstjohn@stanford.edu>
2026-05-14 11:01:50 -0700

    add instructions

--- frontend/src/components/landing/landing.tsx
+++ frontend/src/components/landing/landing.tsx
@@ -3,6 +3,12 @@ import styles from "./landing.module.css";
 export default function DeviceLanding({ onJoin }: { onJoin: () => void }) {
   return (
     <div className={styles.landing}>
+      <ul>
+        <li>Turn ON ringer</li>
+        <li>Set volume to 100%</li>
+        <li>Set brightness to maximum</li>
+        <li>Tap button below!</li>
+      </ul>
       <button onClick={onJoin}>Tap to join</button>
     </div>
   );
